ced9a04d0e1ef0980553c76e9b4ffc56f9855eaa
Charlie Watson <charliewatson87@gmail.com>
2021-08-01 11:32:38 +0100

    feat: add constants for quiz in game.js

---
+++ assets/js/game.js
@@ -0,0 +1,10 @@
+const question = document.getElementById('question');
+const choices = Array.from(document.getElementsByClassName('choice-text'));
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+const correct_
