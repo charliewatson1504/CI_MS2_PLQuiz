6b3f31f52fbc3171f23533b00e0e56b466095048
Charlie Watson <charliewatson87@gmail.com>
2021-08-07 11:12:03 +0100

    feat: update url, host and key in stats.js

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -1,24 +1,11 @@
-let teams = [];
-let names =[];
+result = [];
 
-fetch("https://api-football-v1.p.rapidapi.com/v3/countries", {
+fetch("https://v3.football.api-sports.io/leagues", {
 	"method": "GET",
 	"headers": {
-		"x-rapidapi-key": "6ac09b72d5msh34d2f29a0d7e971p11ca0djsnf30428cae1c6",
-		"x-rapidapi-host": "api-football-v1.p.rapidapi.com"
+		"x-rapidapi-host": "api-football-v1.p.rapidapi.com",
+		"x-rapidapi-key": "6ac09b72d5msh34d2f29a0d7e971p11ca0djsnf30428cae1c6"
 	}
 })
-.then((response) => {
-	return response.json();
-})
-.then((data) => {
-	teams = JSON.parse(data);
-	console.log(teams);
-})
-.then((teams) => {
-	names = teams.response;
-	console.log(names);
-})
-.catch(err => {
-	console.error(err);
-});
+.then((res) => res.json())
+.catch((err) => console.log(err));
