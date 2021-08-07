fdcfc6cc44d84eaf88365b121d2ac584ff396cc4
Charlie Watson <charliewatson87@gmail.com>
2021-08-07 01:04:54 +0100

    style: update styling on feedback modal

---
+++ assets/js/feeback_email.js
@@ -0,0 +1,34 @@
+/*
+Function to sent feedback email using emailjs.com
+The API used is from here - https://www.email.js.com/
+Credit: Code Institute material "Sending Emails Using EmailJS"
+*/
+
+const feedbackFormCall = document.getElementById('feedback-form');
+
+function sendMail (feedbackForm) {
+	emailjs.init('user_qNvMIMV6zD18pi4TUdgwz')
+	emailjs.send('outlook', 'premier_league', {
+		'from_name': feedbackForm.name.value,
+		'from_email': feedbackForm.email.value,
+		'message': feedbackForm.feedback.value
+	}).then(
+		function (response) {
+			success();
+		},
+		function (error) {
+			console.log('Failed to send', error)
+		}
+	);
+	return false;
+};
+
+function success () {
+	feedbackFormCall.innerHTML = 
+	`<div class="modal-body">
+		Thank you for your feedback!
+	</div>
+	<div class="modal-footer">
+		<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+	</div>`
+}
