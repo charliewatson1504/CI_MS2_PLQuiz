3df3fad3a70c4ff2055d82896a08bc6e4a98b19b
Charlie Watson <charliewatson87@gmail.com>
2021-08-08 14:51:25 +0100

    feat: put xml req in a function to allow multiple url calls

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -1,16 +1,16 @@
-const xhr = new XMLHttpRequest();
-
-xhr.onreadystatechange = function(){
-	if(xhr.readyState == 4 && xhr.status == 200){
-		console.log(JSON.parse(xhr.response));
-		data = JSON.parse(xhr.response);
-		console.log(data.response[0]);
-		// stats.innerHTML = `<img src="${data.data.logo_path}">`;
-	}
+function getData(url){
+	return new Promise(function(resolve, reject){
+		const xhr = new XMLHttpRequest();
+		xhr.open("GET", url, true);
+		xhr.setRequestHeader("x-rapidapi-key", "6ac09b72d5msh34d2f29a0d7e971p11ca0djsnf30428cae1c6");
+		xhr.setRequestHeader("x-rapidapi-host", "api-football-v1.p.rapidapi.com");
+		xhr.onload = function(){
+			if(xhr.status == 200){
+				resolve(data = JSON.parse(xhr.response));
+			} else{
+				reject(xhr.statusText);
+			}
+		};
+		xhr.send();
+	});
 };
-
-xhr.open("GET", "https://api-football-v1.p.rapidapi.com/v3/standings?season=2020&league=39");
-xhr.setRequestHeader("x-rapidapi-key", "6ac09b72d5msh34d2f29a0d7e971p11ca0djsnf30428cae1c6");
-xhr.setRequestHeader("x-rapidapi-host", "api-football-v1.p.rapidapi.com");
-
-xhr.send();
