4d9d4e041ce590cb1a5665006b6f582f41944120
Charlie Watson <charliewatson87@gmail.com>
2021-08-09 00:34:31 +0100

    feat: pull answered questions from local storage and display on quiz-end page

--- assets/js/game-end.js
+++ assets/js/game-end.js
@@ -1,4 +1,24 @@
 const finalScoreHtml = document.getElementById('final-score');
+const localStorageTable = document.getElementById('q-and-a');
 const finalScore = localStorage.getItem('mostRecentScore');
+const pullQuestionAndAnswer = localStorage.getItem('qAndA');
+const pulledQuestionAndAnswer = JSON.parse(pullQuestionAndAnswer);
+console.log(pulledQuestionAndAnswer);
 
+let pulledQuestion = pulledQuestionAndAnswer.map(element => element.question);
+let pulledAnswer = pulledQuestionAndAnswer.map(element => element.answer);
+let pulledRightOrWrong = pulledQuestionAndAnswer.map(element => element.rightOrWrong);
+
+localStorageTable.innerHTML = `
+			<tr>
+			<th>Question</th>
+			<th>Answer</th>
+			</tr>`
+			for (i = 0; i < pulledQuestion.length; i++){
+				localStorageTable.innerHTML += `
+				<tr>
+				<td>${pulledQuestion[i]}</td>
+				<td>${pulledAnswer[i]}</td>
+				</tr>`;
+			};
 finalScoreHtml.innerHTML = `Final Score ${finalScore}`;
