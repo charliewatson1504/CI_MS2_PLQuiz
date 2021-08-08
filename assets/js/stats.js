4994250116fd368de2d8f42597ff85eb6ed5c1b5
Charlie Watson <charliewatson87@gmail.com>
2021-08-08 20:19:46 +0100

    feat: change api provider and put elements into table on stats page

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -1,26 +1,28 @@
-function getData(url){
-	return new Promise(function(resolve, reject){
-		const xhr = new XMLHttpRequest();
-		xhr.open("GET", url, true);
-		xhr.setRequestHeader("x-rapidapi-key", "6ac09b72d5msh34d2f29a0d7e971p11ca0djsnf30428cae1c6");
-		xhr.setRequestHeader("x-rapidapi-host", "api-football-v1.p.rapidapi.com");
-		xhr.onload = function(){
-			if(xhr.status == 200){
-				resolve(data = JSON.parse(xhr.response));
-			} else{
-				reject(xhr.statusText);
-			}
-		};
-		xhr.send();
-	});
-};
+const tableBtn = document.getElementById('table-btn');
+const fixturesBtn = document.getElementById('fixtures-btn');
+const tableElement = document.getElementById('table-list')
 
-const promise = getData("https://api-football-v1.p.rapidapi.com/v3/standings?season=2020&league=39");
-promise.then(function(tables){
-	console.log(tables);
-	return getData("https://api-football-v1.p.rapidapi.com/v3/fixtures?league=39&season=2021");
-}).then(function(fixtures){
-	console.log(fixtures);
-}).catch(function(error){
-	console.log(error);
-});
+let rank = [];
+let teams = [];
+let badge = [];
+let fixtures = [];
+
+const xhr = new XMLHttpRequest();
+xhr.open("GET", "https://thesportsdb.p.rapidapi.com/lookuptable.php?s=2020-2021&l=4328");
+xhr.setRequestHeader("x-rapidapi-key", "6ac09b72d5msh34d2f29a0d7e971p11ca0djsnf30428cae1c6");
+xhr.setRequestHeader("x-rapidapi-host", "thesportsdb.p.rapidapi.com");
+xhr.onload = function(){
+	if(xhr.status == 200){
+		let tablesLoaded = JSON.parse(xhr.response);
+		rank = tablesLoaded.table.map(element => element.intRank);
+		teams = tablesLoaded.table.map(element => element.strTeam);
+		badge = tablesLoaded.table.map(element => element.strTeamBadge);
+		for (i = 0; i < rank.length; i++){
+			tableElement.innerHTML += `<td>${rank[i]}</td><td>${teams[i]}</td><td><img src="${badge[i]}"</td>`;
+			}
+			console.log(tablesLoaded);
+	} else{
+		console.log(xhr.statusText);
+	};
+};
+xhr.send();
