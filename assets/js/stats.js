2252001ee934f9ecbbba6f58f94cee6101ebf248
Charlie Watson <charliewatson87@gmail.com>
2021-08-08 14:28:23 +0100

    feat: create xmlhttprequest for stats page

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -1,11 +1,16 @@
-result = [];
+const xhr = new XMLHttpRequest();
 
-fetch("https://v3.football.api-sports.io/leagues", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "api-football-v1.p.rapidapi.com",
-		"x-rapidapi-key": "6ac09b72d5msh34d2f29a0d7e971p11ca0djsnf30428cae1c6"
+xhr.onreadystatechange = function(){
+	if(xhr.readyState == 4 && xhr.status == 200){
+		console.log(JSON.parse(xhr.response));
+		data = JSON.parse(xhr.response);
+		console.log(data.response[0]);
+		// stats.innerHTML = `<img src="${data.data.logo_path}">`;
 	}
-})
-.then((res) => res.json())
-.catch((err) => console.log(err));
+};
+
+xhr.open("GET", "https://api-football-v1.p.rapidapi.com/v3/standings?season=2020&league=39");
+xhr.setRequestHeader("x-rapidapi-key", "6ac09b72d5msh34d2f29a0d7e971p11ca0djsnf30428cae1c6");
+xhr.setRequestHeader("x-rapidapi-host", "api-football-v1.p.rapidapi.com");
+
+xhr.send();
