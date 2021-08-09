541ca73bbfcddc7cc0c2732445176fce96646c45
Charlie Watson <charliewatson87@gmail.com>
2021-08-09 20:21:20 +0100

    fix: add requried semicolons and remove not required ones from game-end.js

--- assets/js/game-end.js
+++ assets/js/game-end.js
@@ -15,7 +15,7 @@ localStorageTable.innerHTML = `
 			<th class="width50"><h1>Question</h1></th>
 			<th class="width25"><h1>Your Answer</h1></th>
 			<th class="width25"><h1>Correct Answer</h1></th>
-			</tr>`
+			</tr>`;
 			for (i = 0; i < pulledQuestion.length; i++){
 				localStorageTable.innerHTML += `
 				<tr>
@@ -23,5 +23,5 @@ localStorageTable.innerHTML = `
 				<td class="width25 ${pulledRightOrWrong[i]}">${pulledAnswer[i]}</td>
 				<td class="width25">${pulledRightAnswer[i]}</td>
 				</tr>`;
-			};
+			}
 finalScoreHtml.innerHTML = `Final Score ${finalScore}`;
