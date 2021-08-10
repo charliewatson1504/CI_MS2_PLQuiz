76895ecfd8b960fb6293805e3ada272f06478596
Charlie Watson <charliewatson87@gmail.com>
2021-08-10 13:53:38 +0100

    feat: add comments to stats.js file

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -1,3 +1,8 @@
+/**
+ * stats.js is used by the stats.html file to load data from
+ * thesportsdb [https://rapidapi.com/theapiguy/api/thesportsdb/] via rapid api [https://rapidapi.com/hub].
+ */
+// stats variables to access DOM elements in the stats.html file
 const tableBtn = document.getElementById('table-btn');
 const table2020Btn = document.getElementById('btn-2020');
 const table2021Btn = document.getElementById('btn-2021');
@@ -14,18 +19,28 @@ let fixtures = [];
 let date = [];
 let time = [];
 let events = [];
-
+/**
+ * event listeners to invoke functions
+ * when the table and fixture buttons are clicked.
+ */
 tableBtn.addEventListener('click', function(){
 	unhide(document.getElementById('seasons'));
 });
-
 table2020Btn.addEventListener('click', getTableData2020);
 table2021Btn.addEventListener('click', getTableData2021);
 fixturesBtn.addEventListener('click', getFixtureData);
 fixturesBtn.addEventListener('click', function(){
 	hide(document.getElementById('seasons'));
 });
-
+/**
+ * getTableData2020 function accesses the api by invoking the XMLHttpRequest.
+ * it uses a key and host to access the data - this is not the most secure way
+ * to store this information so a future improvement would be to store this in 
+ * a backend application.
+ * function checks if a 200 status code is returned then the table data for 2020 season is displayed.
+ * if status code is not 200 then error is logged to the console and error message displayed
+ * to the user.
+ */
 function getTableData2020(){
 	const xhr = new XMLHttpRequest();
 	xhr.open("GET", "https://thesportsdb.p.rapidapi.com/lookuptable.php?s=2020-2021&l=4328");
@@ -62,12 +77,21 @@ function getTableData2020(){
 			}
 		} else{
 			console.log(xhr.statusText);
+			tableElement.innerHTML = `<p>Error with trying to get table information. Please try again.</p>`;
 		}
 	};
 	xhr.send();
 	return ;
 }
-
+/**
+ * getTableData2021 function accesses the api by invoking the XMLHttpRequest.
+ * it uses a key and host to access the data - this is not the most secure way
+ * to store this information so a future improvement would be to store this in 
+ * a backend application.
+ * function checks if a 200 status code is returned then the table data for 2021 season is displayed.
+ * if status code is not 200 then error is logged to the console and error message displayed
+ * to the user.
+ */
 function getTableData2021(){
 	const xhr = new XMLHttpRequest();
 	xhr.open("GET", "https://thesportsdb.p.rapidapi.com/lookuptable.php?s=2021-2022&l=4328");
@@ -102,12 +126,21 @@ function getTableData2021(){
 				}
 		} else{
 			console.log(xhr.statusText);
+			tableElement.innerHTML = `<p>Error with trying to get table information. Please try again.</p>`;
 		}
 	};
 	xhr.send();
 	return ;
 }
-
+/**
+ * getFixtureData function accesses the api by invoking the XMLHttpRequest.
+ * it uses a key and host to access the data - this is not the most secure way
+ * to store this information so a future improvement would be to store this in 
+ * a backend application.
+ * function checks if a 200 status code is returned then the upcoming fixtures are displayed.
+ * if status code is not 200 then error is logged to the console and error message displayed
+ * to the user.
+ */
 function getFixtureData(){
 	const xhr = new XMLHttpRequest();
 	xhr.open("GET", "https://thesportsdb.p.rapidapi.com/eventsnextleague.php?id=4328");
@@ -134,17 +167,25 @@ function getFixtureData(){
 				}
 		} else{
 			console.log(xhr.statusText);
+			tableElement.innerHTML = `<p>Error with trying to get fixture information. Please try again.</p>`;
 		}
 	};
 	xhr.send();
 	return ;
 }
-
+/**
+ * unhide function is to display the 2 season buttons when the table button is clicked.
+ * @param {*} element [the DOM element the function is being called on]
+ */
 function unhide (element) {
 	element.classList.remove('hidden');
 	return ;
 }
-
+/**
+ * hide function is used to hide the 2020 and 2021 buttons when the 
+ * fixtures button is clicked.
+ * @param {*} element [the DOM element the function is being called on]
+ */
 function hide (element) {
 	element.classList.add('hidden');
 	return ;
